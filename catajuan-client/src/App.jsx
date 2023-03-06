5b088b48b2e5f394a0170d731fd0f0fe4d842ba8
Catalina Fernandez <catalinaf97@gmail.com>
2023-03-06 12:07:48 +0100

    routes

--- catajuan-client/src/App.jsx
+++ catajuan-client/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
+import Navigation from './components/Navigation/Navigation'
 import AppRoutes from './routes/AppRoutes'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
       <AppRoutes />
-
+      <Footer />
     </div>
   )
 }
